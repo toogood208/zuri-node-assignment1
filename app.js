ab577572b2fb1146e7458df92af464669048bc58
toogood <tgcode208@gmail.com>
2024-05-09 17:48:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 const http = require("http");
 const os = require("os");
 
-const port = 3000;
-const hostName = '127.0.0.1'
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  // Enable CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    // Handle preflight request
     res.writeHead(200);
     res.end();
     return;
   }
 
-  if (req.url === "/info") {
-    const cpuInfo = os.cpus()[0].model;
-    const osInfo = `${os.type()} ${os.release()}`;
-    const response = { cpuInfo, osInfo };
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(response));
-  } else {
-    res.writeHead(200);
-    res.end(`Visit '${hostName}/${port}/info' to get your pc info`);
-  }
+  setTimeout(() => {
+    if (req.url === "/pc-info") {
+      const cpuInfo = os.cpus()[0].model;
+      const osInfo = `${os.type()} ${os.release()}`;
+      const response = { cpuInfo, osInfo };
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(response));
+    } else {
+      res.writeHead(404);
+      res.end("visit 'http://127.0.0.1:3000/pc-info' to get your PC info");
+    }
+  }, Math.random() * 2000); 
 });
 
-server.listen(port,hostName, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
